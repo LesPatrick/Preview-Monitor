4d0b88f009afcb0213fd4ac35099e17955f63f74
Leszek Barszcz <leszek.barszcz@netguru.com>
2022-12-22 16:49:35 +0100

    usb video autostart

--- usbCameraTest2/src/main/java/com/serenegiant/usbcameratest2/MainActivity.java
+++ usbCameraTest2/src/main/java/com/serenegiant/usbcameratest2/MainActivity.java
@@ -188,6 +188,9 @@ public final class MainActivity extends BaseActivity implements CameraDialog.Cam
 		@Override
 		public void onAttach(final UsbDevice device) {
 			Toast.makeText(MainActivity.this, "USB_DEVICE_ATTACHED", Toast.LENGTH_SHORT).show();
+			if (mUVCCamera == null) {
+				CameraDialog.showDialog(MainActivity.this);
+			}
 		}
 
 		@Override
